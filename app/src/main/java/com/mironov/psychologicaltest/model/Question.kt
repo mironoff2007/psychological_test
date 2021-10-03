99de079d62ebd63af6bc8a9b4fc914eca3e07c9b
mironoff2007 <mironoff2007@rambler.ru>
2021-10-03 12:19:47 +0300

    Init, Model

---
+++ app/src/main/java/com/mironov/psychologicaltest/model/Question.kt
@@ -0,0 +1,15 @@
+package com.mironov.psychologicaltest.model
+
+import android.os.Parcelable
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import kotlinx.android.parcel.Parcelize
+
+@Parcelize
+@Entity(tableName = "question_table")
+data class Question(
+    @PrimaryKey(autoGenerate = true)
+    val id: Int,
+    val text: String,
+    val type: String,
+): Parcelable
