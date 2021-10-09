3ab3242abffbd941283aed623457a4062178bd0c
mironoff2007 <mironoff2007@rambler.ru>
2021-10-09 21:03:54 +0300

    DB table is corrected

--- app/src/main/java/com/mironov/psychologicaltest/model/Question.kt
+++ app/src/main/java/com/mironov/psychologicaltest/model/Question.kt
@@ -1,7 +1,6 @@
 package com.mironov.psychologicaltest.model
 
 import android.os.Parcelable
-import androidx.room.ColumnInfo
 import androidx.room.Entity
 import androidx.room.PrimaryKey
 import kotlinx.android.parcel.Parcelize
