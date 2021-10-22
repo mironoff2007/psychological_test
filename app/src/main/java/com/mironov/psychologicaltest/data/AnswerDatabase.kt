440c1dd8436c0066b338b5119b69be1cf8a559ae
Mironov Ury <mironoff2007@rambler.ru>
2021-10-22 20:28:23 +0300

    DB migration

--- app/src/main/java/com/mironov/psychologicaltest/data/AnswerDatabase.kt
+++ app/src/main/java/com/mironov/psychologicaltest/data/AnswerDatabase.kt
@@ -1,6 +1,7 @@
 package com.mironov.psychologicaltest.data
 
 import android.content.Context
+import androidx.room.AutoMigration
 import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
@@ -8,7 +9,7 @@ import com.mironov.psychologicaltest.model.Answer
 import com.mironov.psychologicaltest.model.TestResult
 
 
-@Database(entities = [Answer::class,TestResult::class], version = 1, exportSchema = false)
+@Database(entities = [Answer::class], version = 1, exportSchema = false)
 abstract class AnswerDatabase : RoomDatabase() {
 
     abstract fun answerDao(): AnswerDao
@@ -30,6 +31,33 @@ abstract class AnswerDatabase : RoomDatabase() {
                 ).setJournalMode(JournalMode.TRUNCATE).build()
                 INSTANCE = instance
                 return instance
+            }
+        }
+    }
+    @AutoMigration(from = 1, to = 2)
+    @Database(entities = [Answer::class,TestResult::class], version = 2, exportSchema = false)
+    abstract class AnswerDatabase : RoomDatabase() {
+
+        abstract fun answerDao(): AnswerDao
+
+        companion object {
+            @Volatile
+            private var INSTANCE: AnswerDatabase? = null
+
+            fun getDatabase(context: Context): AnswerDatabase {
+                val tempInstance = INSTANCE
+                if (tempInstance != null) {
+                    return tempInstance
+                }
+                synchronized(this) {
+                    val instance = Room.databaseBuilder(
+                        context.applicationContext,
+                        AnswerDatabase::class.java,
+                        "psychological_test_answer.db"
+                    ).setJournalMode(JournalMode.TRUNCATE).build()
+                    INSTANCE = instance
+                    return instance
+                }
             }
         }
     }
